135731de0f297e087ef31fc22e62ca6833064219
xnkhi <git.chokehold749@passinbox.com>
2025-10-16 19:18:50 -0400

    uwu

--- home-manager-config/components/files.nix
+++ home-manager-config/components/files.nix
@@ -8,3 +8,4 @@
 	home.file.".config/alacritty".source       = .home-dir-files/config/alacritty;
 	home.file.".config/wallpaper.png".source   = .home-dir-files/config/wallpaper.png;
 	home.file.".chromium-extensions".source    = .home-dir-files/misc/chromium-extensions;
+}
